998203f1d03014a80a0ab68adcf7b49abaa7bc8a
danielcotes2504 <danielenrique2000@hotmail.com>
2021-05-30 23:37:51 -0500

    Initial commit

---
+++ buttons.js
@@ -0,0 +1,77 @@
+function mostrarVoto() {
+    document.getElementById('DivVoto').style.display = 'block';
+    document.getElementById('DivPlebi').style.display = 'none';
+    document.getElementById('DivRefe').style.display = 'none';
+    document.getElementById('DivCons').style.display = 'none';
+    document.getElementById('DivCabi').style.display = 'none';
+    document.getElementById('DivInic').style.display = 'none';
+    document.getElementById('DivRevo').style.display = 'none';
+
+
+}
+
+function mostrarPlebi() {
+    document.getElementById('DivVoto').style.display = 'none';
+    document.getElementById('DivPlebi').style.display = 'block';
+    document.getElementById('DivRefe').style.display = 'none';
+    document.getElementById('DivCons').style.display = 'none';
+    document.getElementById('DivCabi').style.display = 'none';
+    document.getElementById('DivInic').style.display = 'none';
+    document.getElementById('DivRevo').style.display = 'none';
+
+}
+
+function mostrarRefe() {
+    document.getElementById('DivVoto').style.display = 'none';
+    document.getElementById('DivPlebi').style.display = 'none';
+    document.getElementById('DivRefe').style.display = 'block';
+    document.getElementById('DivCons').style.display = 'none';
+    document.getElementById('DivCabi').style.display = 'none';
+    document.getElementById('DivInic').style.display = 'none';
+    document.getElementById('DivRevo').style.display = 'none';
+
+}
+
+function mostrarCons() {
+    document.getElementById('DivVoto').style.display = 'none';
+    document.getElementById('DivPlebi').style.display = 'none';
+    document.getElementById('DivRefe').style.display = 'none';
+    document.getElementById('DivCons').style.display = 'block';
+    document.getElementById('DivCabi').style.display = 'none';
+    document.getElementById('DivInic').style.display = 'none';
+    document.getElementById('DivRevo').style.display = 'none';
+
+}
+
+function mostrarCabi() {
+    document.getElementById('DivVoto').style.display = 'none';
+    document.getElementById('DivPlebi').style.display = 'none';
+    document.getElementById('DivRefe').style.display = 'none';
+    document.getElementById('DivCons').style.display = 'none';
+    document.getElementById('DivCabi').style.display = 'block';
+    document.getElementById('DivInic').style.display = 'none';
+    document.getElementById('DivRevo').style.display = 'none';
+
+}
+
+function mostrarInic() {
+    document.getElementById('DivVoto').style.display = 'none';
+    document.getElementById('DivPlebi').style.display = 'none';
+    document.getElementById('DivRefe').style.display = 'none';
+    document.getElementById('DivCons').style.display = 'none';
+    document.getElementById('DivCabi').style.display = 'none';
+    document.getElementById('DivInic').style.display = 'block';
+    document.getElementById('DivRevo').style.display = 'none';
+
+}
+
+function mostrarRevo() {
+    document.getElementById('DivVoto').style.display = 'none';
+    document.getElementById('DivPlebi').style.display = 'none';
+    document.getElementById('DivRefe').style.display = 'none';
+    document.getElementById('DivCons').style.display = 'none';
+    document.getElementById('DivCabi').style.display = 'none';
+    document.getElementById('DivInic').style.display = 'none';
+    document.getElementById('DivRevo').style.display = 'block';
+
+}
